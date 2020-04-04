6cdb247f6c9bd7c43f8501f83869d60056bf00c2
ZimGil <gil.tichon@gmail.com>
2020-04-04 15:59:49 +0300

    convert to ExpensionPanel

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelAction from '@material-ui/core/ExpansionPanelActions';
 import Checkbox from '@material-ui/core/Checkbox';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import IconButton from '@material-ui/core/IconButton';
 import Delete from '@material-ui/icons/Delete'
 import Edit from '@material-ui/icons/Edit'
@@ -38,17 +40,23 @@ function GetItemList() {
     return <p>Error :(</p>;
   }
   return data.list.map((item) => (
-    <ListItem key={item.id} button disableRipple>
-      <Checkbox
+    <ExpansionPanel key={item.id}>
+      <ExpansionPanelSummary>
+      <FormControlLabel
+        aria-label="Acknowledge"
+        onClick={(event) => event.stopPropagation()}
+        onFocus={(event) => event.stopPropagation()}
+        control={<Checkbox />}
+        onChange={(e) => toggleCompleated({variables: {id: item.id, isCompleated: e.target.checked}})}
         checked={item.isCompleated}
-        onClick={e => e.stopPropagation()}
-        onChange={(e) => toggleCompleated({variables: {id: item.id, isCompleated: e.target.checked}})} />
-      <ListItemText primary={item.title} />
-      <ListItemSecondaryAction>
+        label={item.title}      />
+      </ExpansionPanelSummary>
+      <ExpansionPanelAction onClick={e => e.stopPropagation()} >
         <IconButton size="small"><Edit /></IconButton>
         <IconButton size="small"><Delete /></IconButton>
-      </ListItemSecondaryAction>
-    </ListItem>
+      </ExpansionPanelAction>
+      <ExpansionPanelDetails>GGWP</ExpansionPanelDetails>
+    </ExpansionPanel>
   ));
 }
 
