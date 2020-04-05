771d59b4fdf09f1371bf3e7c5cbb9c9d40157096
ZimGil <gil.tichon@gmail.com>
2020-04-05 16:34:31 +0300

    fix crush, keep warning

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useCallback } from 'react';
+import React, { useState, useMemo } from 'react';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import ExpansionPanel from '@material-ui/core/ExpansionPanel';
 import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
@@ -21,10 +21,7 @@ export default function(props) {
   const [toggleCompleated] = useMutation(TOGGLE_COMPLEATED_MUTATION);
   const [deleteItemMutation] = useMutation(DELETE_ITEM_MUTATION);
   useMemo(() => data && setItemList(data.list), [data]);
-  const updateList = useCallback(() => {
-    setItemList(itemList.concat(props.latestItem))
-  }, [itemList, props.latestItem]);
-  useMemo(() => props.latestItem && updateList(), [props.latestItem, updateList]);
+  useMemo(() => props.latestItem && setItemList(itemList.concat(props.latestItem)), [props.latestItem]);
   if (loading) return <p>Loading...</p>;
   if (error) {
     console.log(error)
